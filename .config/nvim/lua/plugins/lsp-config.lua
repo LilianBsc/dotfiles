8cc2171087f0e08f82d8d09e374149f46405c18b
hke <hke@pascal.inet>
2025-04-07 20:31:50 +0100

    Initialize lsp.

---
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,43 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls" } --"pyright" }
+            })
+        end
+    },
+    {
+        "williamboman/nvim-lsp-installer",
+        config = function()
+            require("nvim-lsp-installer").setup({
+                automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
+                ui = {
+                    icons = {
+                        server_installed = "✓",
+                        server_pending = "➜",
+                        server_uninstalled = "✗"
+                    }
+                }
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            -- lspconfig.pyright.setup({})
+
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+        end
+    }
+}
