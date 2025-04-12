957d4f6aa154b06088952799a4f454f4f35bbe7c
hke <hke@pascal.inet>
2025-04-12 15:09:58 +0100

    Add dashboard.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,12 @@
 return { 
     "folke/tokyonight.nvim",
+    -- "navarasu/onedark.nvim",
     lazy = false,
     priority = 1000,
     opts = {},
     config = function()
+        -- require("onedark").load()
+        -- vim.cmd[[colorscheme onedark]]
         vim.cmd[[colorscheme tokyonight-storm]]
     end
 }
