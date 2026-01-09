a5f388397b22f7492685816e2f44a045265fe2a2
lilian.bosc <you@example.com>
2026-01-09 11:46:21 +0100

    feat: add codediff.nvim and blame.nvim plugins for enhanced git functionality

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,65 +1,99 @@
 return {
-    {
-        "lewis6991/gitsigns.nvim",
-        -- event = "LazyFile",
-        opts = {
-            signs = {
-                add = { text = "▎" },
-                change = { text = "▎" },
-                delete = { text = "" },
-                topdelete = { text = "" },
-                changedelete = { text = "▎" },
-                untracked = { text = "▎" },
-            },
-            signs_staged = {
-                add = { text = "▎" },
-                change = { text = "▎" },
-                delete = { text = "" },
-                topdelete = { text = "" },
-                changedelete = { text = "▎" },
-            },
+  {
+    "lewis6991/gitsigns.nvim",
+    -- event = "LazyFile",
+    opts = {
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+        untracked = { text = "▎" },
+      },
+      signs_staged = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+      },
 
-            on_attach = function(buffer)
-                local gs = package.loaded.gitsigns
+      on_attach = function(buffer)
+        local gs = package.loaded.gitsigns
 
 
-                local function map(mode, l, r, desc)
-                    vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc, silent = true })
-                end
+        local function map(mode, l, r, desc)
+          vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc, silent = true })
+        end
 
-                -- stylua: ignore start
-                map("n", "]h", function()
-                    if vim.wo.diff then
-                        vim.cmd.normal({ "]c", bang = true })
-                    else
-                        gs.nav_hunk("next")
-                    end
-                end, "Next Hunk")
-                map("n", "[h", function()
+        -- stylua: ignore start
+        map("n", "]h", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "]c", bang = true })
+          else
+            gs.nav_hunk("next")
+          end
+        end, "Next Hunk")
+        map("n", "[h", function()
 
-                    if vim.wo.diff then
-                        vim.cmd.normal({ "[c", bang = true })
-                    else
-                        gs.nav_hunk("prev")
-                    end
-                end, "Prev Hunk")
-                -- map("n", "]H", function() gs.nav_hunk("last") end, "Last Hunk")
-                -- map("n", "[H", function() gs.nav_hunk("first") end, "First Hunk")
-                -- map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-                -- map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-                -- map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
-                -- map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
-                -- map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
-                -- map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
-                -- map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
-                -- map("n", "<leader>ghB", function() gs.blame() end, "Blame Buffer")
-                map("n", "<leader>ghd", gs.diffthis, "Diff This")
-                -- map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
-                -- map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
-            end,
-        },
+          if vim.wo.diff then
+            vim.cmd.normal({ "[c", bang = true })
+          else
+            gs.nav_hunk("prev")
+          end
+        end, "Prev Hunk")
+        -- map("n", "]H", function() gs.nav_hunk("last") end, "Last Hunk")
+        -- map("n", "[H", function() gs.nav_hunk("first") end, "First Hunk")
+        -- map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+        -- map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+        -- map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
+        -- map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
+        -- map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
+        -- map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
+        -- map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
+        -- map("n", "<leader>ghB", function() gs.blame() end, "Blame Buffer")
+        map("n", "<leader>ghd", gs.diffthis, "Diff This")
+        -- map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
+        -- map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+      end,
     },
-    {
-        "tpope/vim-fugitive" -- absolute banger
-    },
+  },
+  {
+    "tpope/vim-fugitive" -- absolute banger
+  },
+  {
+    "esmuellert/codediff.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    cmd = "CodeDiff",
+  },
+  {
+
+    "FabijanZulj/blame.nvim",
+    lazy = false,
+    config = function()
+      require('blame').setup {
+        date_format = "%d.%m.%Y",
+        virtual_style = "float",
+        relative_date_if_recent = true, -- this is relative only for the latest month
+        focus_blame = true,
+        merge_consecutive = false,
+        max_summary_width = 30,
+        colors = nil,
+        blame_options = nil,
+        commit_detail_view = "vsplit",
+        format_fn = formats.commit_date_author_fn,
+        mappings = {
+            commit_info = "i",
+            stack_push = "<TAB>",
+            stack_pop = "<BS>",
+            show_commit = "<CR>",
+            close = { "<esc>", "q" },
+        }
+      }
+    end,
+    opts = {
+      blame_options = { '-w' },
+    }
+  },
 }
